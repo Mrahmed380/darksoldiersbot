e585e7d3bea56ff3ccb49fc63d17f07f8bd31f3d
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 13:39:11 -0500

    Update setwelcome.js

--- commands/moderation/setwelcome.js
+++ commands/moderation/setwelcome.js
@@ -5,19 +5,19 @@ module.exports = {
   name: "setwelcome",
   category: "moderation",
   usage: "setwelcome <#channel>",
-  description: "Set the welcome channel",
+  description: "Selecciona el canal de bienvenidas",
   run: (client, message, args) => {
     
     let channel = message.mentions.channels.first()
     
     if(!channel) {
-      return message.channel.send("Please Mention the channel first")
+      return message.channel.send("❌|Primero menciona el canal")
     }
     
     //Now we gonna use quick.db
     
     db.set(`welchannel_${message.guild.id}`, channel.id)
     
-    message.channel.send(`Welcome Channel is seted as ${channel}`)
+    message.channel.send(`✅|El canal de bienvenida está configurado como ${channel}`)
   }
 }
