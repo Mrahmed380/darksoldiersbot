03b5ee1640f44f8f1cf1cd4aa254b90a648ad1f1
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 13:04:20 -0500

    Update addrole.js

--- commands/main/addrole.js
+++ commands/main/addrole.js
@@ -5,14 +5,14 @@ module.exports = {
   aliases: [],
   category: "main",
   usage: "addrole <mention> <role name (don't mention the role)>",
-  description: "Adds a role to the mentioned member.",
+  description: "🚷|Agrega un rol al miembro mencionado.",
   run: async (client, message, args) => {
 
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if (!member)
-      return message.channel.send('Please mention a user or provide a valid user ID');
+      return message.channel.send('❌|Mencione un usuario o proporcione un ID de usuario válido');
     if (member.roles.highest.position >= message.member.roles.highest.position)
-      return message.channel.send('You cannot add a role to someone with an equal or higher role');
+      return message.channel.send('❌|No puede agregar un rol a alguien con un rol igual o superior');
 
     const role = message.guild.roles.cache.find(val => val.name === args[1])
     
@@ -21,16 +21,16 @@ module.exports = {
     if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
 
     if (!role)
-      return message.channel.send('Please mention a role or provide a valid role ID');
+      return message.channel.send('💬|Mencione un rol o proporcione un ID de rol válido');
     else if (member.roles.cache.has(role.id))
-      return message.channel.send('User already has the provided role');
+      return message.channel.send('🔘|El usuario ya tiene el rol proporcionado');
     else {
       try {
 
         await member.roles.add(role);
         const embed = new MessageEmbed()
           .setTitle('Add Role')
-          .setDescription(`${role} was successfully added to ${member}.`)
+          .setDescription(`${role} fue agregado exitosamente a ${member}.`)
           .addField('Moderator', message.member, true)
           .addField('Member', member, true)
           .addField('Role', role, true)
