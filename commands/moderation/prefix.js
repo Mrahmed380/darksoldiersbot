ebdbf75ade3c91f70ed6fa95393e3801bb3237e4
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 13:33:50 -0500

    Update prefix.js

--- commands/moderation/prefix.js
+++ commands/moderation/prefix.js
@@ -2,35 +2,35 @@ const db = require("quick.db")
 const { default_prefix } = require("../../config.json")
 
 module.exports = {
-  name: "prefix",
+  name: "setprefix",
   category: "moderation",
-  usage: "prefix <new-prefix>",
-  description: "Change the guild prefix",
+  usage: "setprefix <new-prefix>",
+  description: "Cambia el prefix del bot",
   run: async (client, message, args) => {
     //PERMISSION
     if(!message.member.hasPermission("ADMINISTRATOR")) {
-      return message.channel.send("You are not allowed or do not have permission to change prefix")
+      return message.channel.send("❌|No tienes permiso para cambiar el preifx")
     }
     
     if(!args[0]) {
-      return message.channel.send("Please give the prefix that you want to set")
+      return message.channel.send("❌|Dime el nombre que quieras cambiar en el prefix")
     } 
     
     if(args[1]) {
-      return message.channel.send("You can not set prefix a double argument")
+      return message.channel.send("❌|No se puede establecer como prefix un argumento doble")
     }
     
     if(args[0].length > 3) {
-      return message.channel.send("You can not send prefix more than 3 characters")
+      return message.channel.send("❌|No se puede enviar prefijo de más de 3 caracteres")
     }
     
     if(args.join("") === default_prefix) {
       db.delete(`prefix_${message.guild.id}`)
-     return await message.channel.send("Reseted Prefix ✅")
+     return await message.channel.send("✅|Prefix reseteado")
     }
     
     db.set(`prefix_${message.guild.id}`, args[0])
-  await message.channel.send(`Seted Bot Prefix to ${args[0]}`)
+  await message.channel.send(`✅|Prefix puesto con exito ${args[0]}`)
     
   }
 }
