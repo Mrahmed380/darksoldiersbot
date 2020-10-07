7f10602c6c4f3a65650dd270764e8f81a10b125d
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 13:17:21 -0500

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -3,40 +3,40 @@ const discord = require("discord.js");
 module.exports = {
   name: "ban",
   category: "moderation",
-  description: "Ban anyone with one shot whithout knowing anyone xD",
+  description: "💫|Banear a cualquiera de un disparo sin conocer a nadie xD",
   usage: "ban <@user> <reason>",
   run: async (client, message, args) => {
     
     if(!message.member.hasPermission("BAN_MEMBERS")) {
-      return message.channel.send(`**${message.author.username}**, You do not have perms to ban someone`)
+      return message.channel.send(`**${message.author.username}**, ❌|No tienes permiso para banear.`)
     }
     
     if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
-      return message.channel.send(`**${message.author.username}**, I am do not have perms to ban someone`)
+      return message.channel.send(`**${message.author.username}**, ❌|No tengo permisos para banear a alguien`)
     }
     
     const target = message.mentions.members.first();
     
     if(!target) {
-      return message.channel.send(`**${message.author.username}**, Please mention the person who you want to ban.`)
+      return message.channel.send(`**${message.author.username}**, ❌|Menciona a una persona que quieras banear.`)
     }
     
     if(target.id === message.author.id) {
-      return message.channel.send(`**${message.author.username}**, You can not ban yourself!`)
+      return message.channel.send(`**${message.author.username}**, ❌|No te puedes banear a ti mismo!`)
     }
     
    
     
    if(!args[1]) {
-     return message.channel.send(`**${message.author.username}**, Please Give Reason To ban Member`)
+     return message.channel.send(`**${message.author.username}**, ❌|Indique la razón para banear el miembro`)
    }
     
     let embed = new discord.MessageEmbed()
     .setTitle("Action : Ban")
-    .setDescription(`Banned ${target} (${target.id})`)
+    .setDescription(`Baneado ${target} (${target.id})`)
     .setColor("#ff2050")
     .setThumbnail(target.avatarURL)
-    .setFooter(`Banned by ${message.author.tag}`);
+    .setFooter(`Baneado por ${message.author.tag}`);
     
     message.channel.send(embed)
     target.ban(args[1])
