7efddff28611bcd9815f994da2abd917fed3f937
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 11:55:14 -0500

    Update status.js

--- commands/creator/status.js
+++ commands/creator/status.js
@@ -3,14 +3,14 @@ const discord = require("discord.js")
 
 module.exports = {
   name: "status",
-  description: "Change the bot status",
+  description: "✋|Cambiar el estado del bot",
   usage: "status <here>",
   category: "owner",
   run: async (client, message, args) => {
     
     //OWNER ONLY COMMAND
     if(!message.author.id === "452921092961468417") {
-      return message.channel.send("This command can only be used by owner")
+      return message.channel.send("✅ |Este comando solo puede ser utilizado por el creador")
     }
     //ARGUMENT
     if(!args.length) {
