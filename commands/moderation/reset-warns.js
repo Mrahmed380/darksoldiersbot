de9530f2516a4c24b9b14104e0687085c49b257d
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 13:37:49 -0500

    Update reset-warns.js

--- commands/moderation/reset-warns.js
+++ commands/moderation/reset-warns.js
@@ -2,38 +2,38 @@ const db = require("quick.db")
 
 module.exports = {
   name: "resetwarns",
-  aliases: ["rwarns"],
-  usage: "rwarns <@user>",
-  description: "Reset warnings of mentioned person",
+  aliases: ["resetwarns"],
+  usage: "resetwarns <@user>",
+  description: "🚀|Resetea los warns",
   run: async (client, message, args) => {
     
     
     if(!message.member.hasPermission("ADMINISTRATOR")) {
-      return message.channel.send("Yopu should have admin perms to use this command")
+      return message.channel.send("❌|Debe tener permisos de administrador para usar este comando")
     }
     
     const user = message.mentions.members.first()
     
     if(!user) {
-    return message.channel.send("Please mention the person whose warning you want to reset")
+    return message.channel.send("❌|Menciona un usuario al cual quieras resetear sus warns")
     }
     
     if(message.mentions.users.first().bot) {
-      return message.channel.send("Bot are not allowed to have warnings")
+      return message.channel.send("❌|Los bot no pueden tener advertencias")
     }
     
     if(message.author.id === user.id) {
-      return message.channel.send("You are not allowed to reset your warnings")
+      return message.channel.send("❌|No se le permite restablecer sus WARNS")
     }
     
     let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
     
     if(warnings === null) {
-      return message.channel.send(`${message.mentions.users.first().username} do not have any warnings`)
+      return message.channel.send(`${message.mentions.users.first().username} ❌|No tiene ningun warn`)
     }
     
     db.delete(`warnings_${message.guild.id}_${user.id}`)
-    user.send(`Your all warnings are reseted by ${message.author.username} from ${message.guild.name}`)
+    user.send(`✅|Todos tus warns son reiniciados por ${message.author.username} desde ${message.guild.name}`)
     await message.channel.send(`Reseted all warnings of ${message.mentions.users.first().username}`)
     
   
