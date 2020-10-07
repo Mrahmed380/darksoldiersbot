4a08c42624d4fb0216d55a2fd8962236fd780e35
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 13:25:05 -0500

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -3,37 +3,37 @@ const discord = require("discord.js");
 module.exports = {
   name: "kick",
   category: "moderation",
-  description: "Kick anyone with one shot xD",
+  description: "🆗|Kickea a cualquiera con un solo disparo xD",
   usage: "kick <@user> <raeson>",
   run: (client, message, args) => {
     
     if(!message.member.hasPermission("KICK_MEMBERS")) {
-      return message.channel.send(`**${message.author.username}**, You do not have enough permission to use this command`)
+      return message.channel.send(`**${message.author.username}**, ❌|No tienes permiso suficiente para usar este comando`)
     }
     
     if(!message.guild.me.hasPermission("KICK_MEMBERS")) {
-      return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`)
+      return message.channel.send(`**${message.author.username}**, ❌|No tengo permiso suficiente para ejecutar este comando`)
     }
     
     let target = message.mentions.members.first();
     
     if(!target) {
-      return message.channel.send(`**${message.author.username}**, Please mention the person who you want to kick`)
+      return message.channel.send(`**${message.author.username}**, ❌|Por favor mencione a la persona a la que quiere kickear`)
     }
     
     if(target.id === message.author.id) {
-     return message.channel.send(`**${message.author.username}**, You can not kick yourself`)
+     return message.channel.send(`**${message.author.username}**, ❌|No te puedes kickear a ti mismo`)
     }
     
   if(!args[1]) {
-    return message.channel.send(`**${message.author.username}**, Please Give Reason to ban`)
+    return message.channel.send(`**${message.author.username}**, ❌|Dime una razon para kickear`)
   }
     
     let embed = new discord.MessageEmbed()
     .setTitle("Action: Kick")
     .setDescription(`Banned ${target} (${target.id})`)
     .setColor("#ff2050")
-    .setFooter(`Banned by ${message.author.username}`);
+    .setFooter(`Kickeado por ${message.author.username}`);
     
     message.channel.send(embed)
     
