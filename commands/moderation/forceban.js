aab8c5fe67ec1c7975dfc64182a180c2e2ab88d5
elevierrex <50177004+elevierrex@users.noreply.github.com>
2020-10-07 13:22:11 -0500

    Update forceban.js

--- commands/moderation/forceban.js
+++ commands/moderation/forceban.js
@@ -2,19 +2,19 @@ module.exports = {
   name: "forceban",
   usage: "forceban [id]",
   aliases: ["fb"],
-  description: "Ban anyone by id, you can ban him him if he is not in the server by his id",
+  description: "Banear a cualquier persona por identificación, puede banearlo si no está en el servidor por su identificación",
   category: "moderation",
   run: (client, message, args) => {
     if (!message.member.hasPermission("BAN_MEMBERS")) {
-            return message.reply("❌ You do not have permissions to ban members. Please contact a staff member");
+            return message.reply("❌|No tienes permisos para banear miembros. Comuníquese con un miembro del personal");
    }
-    if (!args[0]) return message.channel.send( {embed: { "description": "Please type the id", "color": 0xff2050 } });
+    if (!args[0]) return message.channel.send( {embed: { "description": "❌|Dime la id", "color": 0xff2050 } });
     let dead = args[0];
     message.guild.ban(dead).then(() => {
-        message.channel.send({embed: { "description": `\`${args[0]}\` Has been force banned from the server`, color: 0xff2050 } })
+        message.channel.send({embed: { "description": `\`${args[0]}\` ✅|Se a utilizado forceban con exito`, color: 0xff2050 } })
     
     }).catch(err => {
-        message.channel.send( { embed:{ "description": "The ID `"+message.args[0]+"` is not a valid user.", "color": 0xff2050 } });
+        message.channel.send( { embed:{ "description": "The ID `"+message.args[0]+"` ❌|No es un usuario valido.", "color": 0xff2050 } });
     })
 }
 }
